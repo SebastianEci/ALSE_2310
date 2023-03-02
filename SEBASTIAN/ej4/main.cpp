7fe1c4eeb02e5cfbef3feaa18c7e1476a8022fd2
SebastianECI <sebastian.ramirez-m@mail.escuelaing.edu.co>
2023-03-01 21:08:13 -0500

    Ejercicio sumatoria de los 10 datos

--- SEBASTIAN/ej4/main.cpp
+++ SEBASTIAN/ej4/main.cpp
@@ -12,9 +12,18 @@ float promedio( int v[] ){
   return prom / 10.;
 }
 
+float sumatoria (int  v[]){
+	float sumaT =0.;
+	for( int i = 0; i < 10; i++){
+	sumaT += v[i];
+ }
+return sumaT;
+}
+
 int main(){
   int vec[10];
   float prom=0.;
+  float suma=0.;
 
   cout << "Ingresar diez (10) número enteros: ";
 
@@ -23,7 +32,9 @@ int main(){
   }
 
   prom = promedio( vec );
-
+  suma = sumatoria (vec);
   cout << "El promedio de los datos ingresados es: " << prom << "\n";
+  cout << "El Sumatoria de los datos ingresados es: " << suma << "\n";
+
   return 0;
 }
