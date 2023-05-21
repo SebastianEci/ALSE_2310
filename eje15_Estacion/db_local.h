7145cd9de0411dafd40d947c4cb268907fed222b
Alexander Pérez <alexander.perez@escuelaing.edu.co>
2023-05-21 08:11:08 -0500

    Agregada la función de delay.

--- eje15_Estacion/db_local.h
+++ eje15_Estacion/db_local.h
@@ -1,8 +1,16 @@
 #include "medicion.h"
+#include <string>
 
+using namespace std;
 
 class DB_Local{
 
+public:
+  DB_Local( string url );
+  bool Abrir_DB();
+  bool Ingresar( const Medicion &med );
+  bool Recuperar( Medicion &prom );
+  bool Cerar_DB();
 
 
 
